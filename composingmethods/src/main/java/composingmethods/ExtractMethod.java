75e2752b599f64609dc3a6046d3279ea751c9e47
Shao Wei <lshaowei18@gmail.com>
2019-06-17 13:55:53 +0800

    Starting code

---
+++ composingmethods/src/main/java/composingmethods/ExtractMethod.java
@@ -0,0 +1,24 @@
+package composingmethods;
+
+public class ExtractMethod {
+
+  void printOwing(String name ,double amount) {
+    Enumeration e = _orders.elements();
+    double outstanding = 0.0;
+
+    //print banner
+    System.out.println ("**************************");
+    System.out.println ("***** Customer Owes ******");
+    System.out.println ("**************************");
+
+    //calculate outstanding
+    while(e.hasMoreElements()) {
+      Order each = (Order) e.nextElement();
+      outstanding += each.getAmount();
+    }
+
+    //print details
+    System.out.println("name:" + _name);
+    System.out.println("amount" + outstanding);
+  }
+}
