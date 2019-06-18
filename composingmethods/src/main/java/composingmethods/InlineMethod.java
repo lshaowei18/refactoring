470d42c1d3152c2eec218d2059406b614a30d738
Shao Wei <lshaowei18@gmail.com>
2019-06-18 17:10:16 +0800

    Starting code

---
+++ composingmethods/src/main/java/composingmethods/InlineMethod.java
@@ -0,0 +1,14 @@
+package composingmethods;
+
+public class InlineMethod {
+  private int _quantity;
+  private int _itemPrice;
+
+  double getPrice() {
+    int basePrice = _quantity * _itemPrice;
+    double discountFactor;
+    if (basePrice > 1000) discountFactor = 0.95;
+    else discountFactor = 0.98;
+    return basePrice * discountFactor;
+  }
+}
